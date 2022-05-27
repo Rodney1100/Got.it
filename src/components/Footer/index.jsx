c8fe684029e7c60ec1e084944691456a502c90f8
rodney1100 <rodney.plancher@gmail.com>
2022-05-27 01:44:13 -0400

    added the footer and I need to turn the buttons on

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,42 +1,94 @@
 import React from "react";
+// import MdEmail from "react-icons/md";
+import MarkunreadIcon from '@mui/icons-material/Markunread';
+import {GrMail}  from "react-icons/gr";
 import styled from "styled-components";
-import { FaFacebook, FaGithub, FaLinkedinIn } from "react-icons/fa";
+import {
+  FaMailBulk,
+  FaFacebook,
+  FaGithub,
+  FaLinkedinIn,
+  FaMapMarker,
+  FaPhoneAlt,
+  FaCcAmex,
+  FaCcVisa,
+  FaCcMastercard,
+} from "react-icons/fa";
+
 const Container = styled.div`
   display: flex;
   background-color: #c19c6b49;
 `;
+
 const Left = styled.div`
   flex: 1;
   display: flex;
   flex-direction: column;
   padding: 20px;
 `;
+
+const Logo = styled.h1``;
+
 const Center = styled.div`
   flex: 1;
+  padding: 20px;
 `;
+
 const Right = styled.div`
   flex: 1;
+  padding: 20px;
 `;
-const Logo = styled.h1`
-  font-weight: bold;
-`;
+
 const SocialMedia = styled.div`
   display: flex;
 `;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Title = styled.h3`
+  margin-bottom: 20px;
+`;
+
 const SocialMediaIcon = styled.div`
   margin: 2px;
-  width: 40px;
-  height: 40px;
+  width: 30px;
+  height: 30px;
   border-radius: 50%;
   color: #ffffff;
   background-color: #${(props) => props.color};
   justify-content: center;
   align-items: center;
   display: flex;
-  font-size: 25px;
+  font-size: 20px;
 `;
-const Desc = styled.div`
+
+const Desc = styled.p`
   margin: 20px 0px;
+`;
+
+const ContactInfo = styled.div``;
+const ContactList = styled.div`
+  margin-bottom: 20px;
+  font-size: medium;
+`;
+const ContactList1 = styled.div`
+  /* margin-bottom: 10px; */
+  /* display: inline-block; */
+  font-size: medium;
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
 `;
 
 export const Footer = () => {
@@ -45,28 +97,59 @@ export const Footer = () => {
       <Left>
         <Logo className="logo">GOT.It</Logo>
         <Desc>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates,
-          amet error eum culpa dolore dolorem cum esse! Nulla saepe quia quo
-          obcaecati ut itaque architecto hic provident, quas facilis officiis
-          porro alias magni rerum praesentium modi laboriosam odit, vero, neque
-          vitae aspernatur iste cupiditate labore! Veritatis esse eligendi iusto
-          repellat dicta explicabo, rerum cumque, alias excepturi, nam accusamus
-          odio!
+          This is NOT a Real website; it is a Demo E-commerce website created by
+          Rodney Plancher. You can use the Demo card to check out the Purchase
+          on this Project.
         </Desc>
         <SocialMedia>
+          <SocialMediaIcon color="3B5999">
+            <FaFacebook />
+          </SocialMediaIcon>
           <SocialMediaIcon color="E60023">
             <FaLinkedinIn />
           </SocialMediaIcon>
           <SocialMediaIcon color="000000">
             <FaGithub />
           </SocialMediaIcon>
-          <SocialMediaIcon color="3B5999">
-            <FaFacebook />
-          </SocialMediaIcon>
         </SocialMedia>
       </Left>
-      <Center></Center>
-      <Right></Right>
+      <Center>
+        <Title>Usefull Links</Title>
+        <List>
+          <ListItem> Men's Fashion </ListItem>
+          <ListItem> Women's Fashion </ListItem>
+          <ListItem> Order Tracking </ListItem>
+          <ListItem> Home </ListItem>
+          <ListItem> Wishlist </ListItem>
+          <ListItem> Cart </ListItem>
+          <ListItem> My Account </ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <ContactInfo>
+          <Title>Contact</Title>
+          <ContactList>
+            <FaMapMarker style={{ marginRight: "10px" }} />
+            2040 Delmar Ave, Vero Beach, FL 32960
+          </ContactList>
+          <ContactList>
+            <FaPhoneAlt style={{ marginRight: "10px" }} />
+            555-555-5555
+          </ContactList>
+          <ContactList1>
+              <MarkunreadIcon style={{ marginRight: "10px", fontSize:"larger"}} />
+              Rodney.plancher@yahoo.com
+          </ContactList1>
+          <ContactList1>
+              <FaCcAmex  style={{ marginRight: "10px", fontSize:"larger"}} />
+              <FaCcVisa style={{ marginRight: "10px", fontSize:"larger"}} />
+            <FaCcMastercard style={{ marginRight: "10px", fontSize: "larger" }} />
+            We accept all Major Credit cards 
+          </ContactList1>
+   
+        </ContactInfo>
+      </Right>
     </Container>
   );
 };
--- src/components/NewsLetters/index.jsx
+++ src/components/NewsLetters/index.jsx
@@ -14,17 +14,18 @@ const Title = styled.div`
   margin: 3px;
   font-size: 25px;
   font-weight: bold;
-  `;
+`;
 const Description = styled.div`
   margin: 3px;
   font-weight: bold;
   font-size: 17px;
-  `;
+`;
 const Button = styled.button`
   flex: 1;
   font-size: 30px;
-  background-color: #a5e4e1;
-
+  background-color: #91eee9;
+  border: none;
+  cursor: pointer;
 `;
 const Input = styled.input`
   flex: 8;
@@ -33,8 +34,8 @@ const Input = styled.input`
 
 const InputContainer = styled.div`
   margin: 3px;
-  width: 31%;
-  height: 40px;
+  width: 40%;
+  height: 35px;
   font-size: 20px;
   background-color: #4798b313;
   display: flex;
@@ -50,7 +51,7 @@ export const NewsLetter = () => {
         Please enter your email to find out with our Newsletter
       </Description>
       <InputContainer>
-        <Input placeholder="Email Here"/>
+        <Input placeholder="Email Here" />
         <Button>
           <FaTelegramPlane color="gray" />
         </Button>
